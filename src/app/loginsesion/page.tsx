9d85759d7f94bbb2f903a4ae11aa1ec15320d888
1rodrig0 <rodrigo.04as@gmail.com>
2025-04-14 22:55:37 -0400

    Sin probar

---
+++ src/app/loginsesion/page.tsx
@@ -0,0 +1,18 @@
+    import React from 'react';
+    import Image from 'next/image';
+    import styles from './eventos.module.css';
+    import Login from '@/components/Login';
+
+
+
+
+    const LoginPage = () => {
+    return (
+        <div><Login /></div>
+    
+
+        
+    );
+    };
+
+    export default LoginPage;
